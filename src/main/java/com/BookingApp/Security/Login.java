17721750e33f928c322b5b40938ff3d17d3942d1
alex <firica.alexandru@yahoo.com>
2023-08-07 22:03:05 +0300

    Add Create User Account button and Create Accommodation class button to login page

--- src/main/java/com/BookingApp/Security/Login.java
+++ src/main/java/com/BookingApp/Security/Login.java
@@ -1,10 +1,10 @@
 package com.BookingApp.Security;
 
-import com.vaadin.flow.component.html.Div;
+import com.BookingApp.Views.Manager.CreateAccomodation;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginForm;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.*;
 
@@ -24,12 +24,23 @@ public class Login extends VerticalLayout implements BeforeEnterListener {
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
 
+//        Button createAccomodation = new Button("Create Accommodation Account");
+//        createAccomodation.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SUCCESS);
+//
+//        Button createUsers = new Button("Create User Account");
+//        createUsers.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SUCCESS);
+
+
         loginForm.setAction("login");
+
         add(
                 new H1("BookingApp Login"),
                 loginForm);
 
         loginForm.getElement().setAttribute("no-autofocus", "");
+//        add(createUsers,createAccomodation);
+        add( createButton("Create User Account", CreateAccomodation.class),
+                createButton("Create Accommodation Account", CreateAccomodation.class));
 
     }
 
@@ -41,5 +52,18 @@ public class Login extends VerticalLayout implements BeforeEnterListener {
                 .containsKey("error")){
             loginForm.setError(true);
         }
+    }
+
+    private Button createButton(String buttonName, Class cls) {
+        RouterLink link = new RouterLink();
+        link.add(buttonName);
+        link.setRoute(cls);
+        link.setTabIndex(-1);
+        Button button = new Button(link);
+        button.addThemeVariants(
+                ButtonVariant.LUMO_TERTIARY
+        );
+
+        return button;
     }
 }
