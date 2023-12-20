205d53896a8c6b045a26682d3d6bb27fd230ec53
alexandrufirica <firica.alexandru@yahoo.com>
2023-12-20 16:11:27 +0200

    Deoloyed in Azure with a SQl database #2

---
+++ frontend/generated/flow/chunks/chunk-09f3f32cc53e2debb86a1e6bb11a58fc4fada540cd42d4ee770c8928a9b6d856.js
@@ -0,0 +1,9 @@
+import '@vaadin/tooltip/theme/lumo/vaadin-tooltip.js';
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/checkbox/theme/lumo/vaadin-checkbox.js';
+import '@vaadin/button/theme/lumo/vaadin-button.js';
+import 'Frontend/generated/jar-resources/buttonFunctions.js';
+import '@vaadin/login/theme/lumo/vaadin-login-form.js';
+import '@vaadin/notification/theme/lumo/vaadin-notification.js';
+import 'Frontend/generated/jar-resources/flow-component-renderer.js';
+import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
